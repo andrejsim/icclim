38c204f00df5c4d258bacb28fe4f5d4c94eb2663
Natalia Tatarinova <sacora@gmail.com>
2015-10-20 11:26:53 +0200

    updated version number

--- icclim/icclim_doc/source/conf.py
+++ icclim/icclim_doc/source/conf.py
@@ -59,7 +59,7 @@ copyright = u'2015, CERFACS'
 # built documents.
 #
 # The short X.Y version.
-version = '4.0.3'
+version = '4.1.0-test'
 # The full version, including alpha/beta/rc tags.
 release = version
 
